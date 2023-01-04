e85242cb83c4958badd8993915d1da02d2b17436
Ismail Tiamiyu <tiami.abiola@gmail.com>
2023-01-04 22:44:01 +0100

    committing  new change

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { createContext } from "react";
 function App() {
 	const themeContext = createContext("null");
 
-	const [theme, setTheme] = useState("light");
+	const [theme, setTheme] = useState("dark");
 	function toggle() {
 		setTheme(theme === "dark" ? "light" : "dark");
 	}
