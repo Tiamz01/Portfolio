b8186ddbce01850f431a6a355e195b0d3b22f4b7
Ismail Tiamiyu <tiami.abiola@gmail.com>
2023-01-27 15:43:12 +0100

    fixed errroes

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,29 +4,15 @@ import Title from "../utils/Title.jsx";
 
 const About = () => {
 	return (
-		<section
-			id='about-me'
-			data-aos='fade-down'
-		>
+		<section id='about-me' data-aos='fade-down'>
 			<div className=''>
 				{about.map((val, i) => (
-					<div
-						key={i}
-						className='grid items-center '
-					>
-						<h1
-							className=''
-							data-aos='fade-left'
-						>
-							<Title
-								title={val.title}
-								style={{ margin: "1rem  0" }}
-							/>
+					<div key={i} className='grid items-center '>
+						<h1 className='' data-aos='fade-left'>
+							<Title title={val.title} style={{ margin: "1rem  0" }} />
 						</h1>
 
-						<p className='max-w-[40rem] ml-3 text-white font-poppins text-justify text-lg sm:text-base sm:ml-0 md:text-[1rem] mt-[3rem] box-content px-[2rem] xsm:text-[.8rem]'>
-							{val.text}
-						</p>
+						<p className='max-w-[40rem] text-white font-poppins text-justify text-lg sm:text-base sm:ml-0 md:text-[1rem] mt-[3rem] box-content px-[2rem] xsm:text-[.8rem]'>{val.text}</p>
 					</div>
 				))}
 			</div>
