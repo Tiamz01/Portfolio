98d8bf12db6ade51152e7be367ca60edf554d946
tiamz <tiami.abiola@gmail.com>
2025-10-06 09:19:37 +0100

    Fix ECS import and update portfolio data

--- src/data/index.js
+++ src/data/index.js
@@ -18,21 +18,19 @@ const infos = [
     {
         img: Tiamz,
         intro: "hey, I am Ismail",
-        text: "Frontend developer with experience in creating responsive and interactive web applications."
+        text: "Frontend developer with experience in intergrating ML/AI model in creating responsive and interactive web applications."
     }
 ];
 
 const about = [
     {
         title: "About Me",
-        text: "I am a passionate Front-End Developer skilled in HTML, CSS, JavaScript, and modern frameworks like React and Remix. \
-		I am specialized in building responsive, accessible, and high-performance web applications that deliver seamless user experiences. \
-		I have hands-on experience turning complex designs into interactive, user-friendly interfaces using Tailwind CSS, TypeScript, \
-		and modern UI best practices. My focus is always on creating clean, maintainable code while optimizing for speed, scalability, \
-		and accessibility. I’m deeply committed to continuous learning and staying ahead of emerging trends in the frontend ecosystem. \
-		My goal is to become a well-rounded frontend engineer, mastering everything from UI implementation and state management to \
-		API integration and performance optimization. I believe persistence, creativity, and curiosity are the driving forces behind \
-		impactful web solutions and I bring those values to every project I work on."
+        text: "I’m a passionate Frontend Developer with a strong foundation in JavaScript, React, and modern UI frameworks, and a growing focus on integrating AI and Machine Learning into user experiences. \
+		My background in Data Science and Machine Learning allows me to design not only responsive and interactive interfaces but also intelligent applications that deliver data-driven insights in real time.\
+		I enjoy building products where data meets design from dashboards that visualize predictive analytics to interfaces that interact seamlessly with AI-powered APIs. With hands-on experience in Python, \
+		Scikit-learn, Flask, and RESTful API integration, I bridge frontend engineering with backend intelligence, creating smart, efficient, and human-centered digital solutions.\
+		I’m committed to continuous learning, exploring emerging technologies like LLMs, data visualization, and AI-assisted UX, and collaborating with teams to push the boundaries \
+		of what’s possible on the web."
     }
 ];
 const portfolio = [
@@ -74,6 +72,21 @@ const portfolio = [
 				url: "https://sneakernews.com/2022/09/14/air-jordan-37-low/",
 				btn: "View now"
 			},
+			 {
+				title: "Book recommendation system",
+				text: "The project focused on suggesting books based on close proximity to the intended search using K Nearest Neigbor",
+				img: BKS,
+				url: "https://bookrecommendationsystem-alsqf8anh38pyvwutmjv6a.streamlit.app/",
+				btn: "View now"
+			},
+			{
+			
+				title: "Email/SMS classificaition system",
+				text: "The project focused on classify in incoming sms or email as either spam or not. It is Machine learning classification algorithm",
+				img: ECS,
+				url: "https://emailspamclassification-sb4xawajku9yn6wyahuk4f.streamlit.app/",
+				btn: "View now"
+			},
 			// {
 			// 	title: "Student Performance Prediction",
 			// 	text: "The project focused on Predicting student score using machine learning",
@@ -81,25 +94,12 @@ const portfolio = [
 			// 	url: "https://github.com/Tiamz01/ML_Student_Performance_Predictor",
 			// 	btn: "View now"
 			// },
-			// {
-			// 	title: "Email/SMS classificaition system",
-			// 	text: "The project focused on classify in incoming sms or email as either spam or not. It is Machine learning classification algorithm",
-			// 	img: ECS,
-			// 	url: "https://emailspamclassification-sb4xawajku9yn6wyahuk4f.streamlit.app/",
-			// 	btn: "View now"
-			// },
+			
 			// {
 			// 	title: "Absenteeism Prediction",
 			// 	text: "The project focused on Predicting and understand the  factor for staff absenteeism",
 			// 	img: Absenteeism,
 			// 	url: "https://github.com/Tiamz01/Absenteeism_project",
-			// 	btn: "View now"
-			// },
-			// {
-			// 	title: "Book recommendation system",
-			// 	text: "The project focused on suggesting books based on close proximity to the intended search using K Nearest Neigbor",
-			// 	img: BKS,
-			// 	url: "https://bookrecommendationsystem-alsqf8anh38pyvwutmjv6a.streamlit.app/",
 			// 	btn: "View now"
 			// },
 			// {
